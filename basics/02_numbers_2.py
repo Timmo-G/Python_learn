b1776241cf4831acbbe7117601a119ec4c86b05f
Timmo-G <gdy200212310010@163.com>
2026-04-28 21:44:18 +0800

    feat:numbers and mathematical operation

---
+++ basics/02_numbers_2.py
@@ -0,0 +1,8 @@
+#扩充赋值 variable <operator>= value
+
+# 扩充赋值对于字符串只有+= *=
+greet = 'hello'
+greet *= 3 # hellohellohello
+
+# ++x不会增加x 需要写 x += 1
+
---
+++ basics/02_numbers_float_1.py
@@ -0,0 +1,25 @@
+# 地板除法 //
+
+# 幂运算 **
+
+# float（）
+m_int = 985
+m_float = float(m_int)
+print(m_float)
+print(type(m_float))
+
+# 四舍五入round()
+int_1 = 985.211
+
+rounded_int_1 = round(int_1)# 985
+rounded_int_2 = round(int_1,1)# 985.2
+
+# abs()绝对值
+int_2 = -211
+abs_int_1 = abs(int_2)#211
+
+# 幂运算 POW()
+int_1 = pow(2,3)# == 2 ** 3
+print(int_1) # 8
+
+
